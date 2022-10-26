fb0e05a96d72c53194b4bdc10e1df5d893ec973d
Carlos Javier <cjgomezm01@informatica.iesvalledeljerte.es>
2022-10-26 20:02:01 +0200

    Subida index Carlos J Gomez

---
+++ indexCarlos.php
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+ <head>
+  <title>Carlos Javier Gomez Moreno</title>
+ </head>
+ <body>
+ <?php echo '<p>Hola Mundo</p> <p> Este es una prueba de Carlos J Gomez </p>'; ?>
+ </body>
+</html>
